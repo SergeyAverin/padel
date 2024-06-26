8cbe639886501e2c919c4782adbd262e2d098685
SergeyAverin <ct.averin@yandex.ru>
2024-06-26 15:38:02 +0600

    fix(backend): fixed get user frinds

--- backend/friends/routes.py
+++ backend/friends/routes.py
@@ -25,4 +25,4 @@ async def create_friend_request(recipient_user_id: str = Body()):
 @friend_router.get('/friends/{user_id}')
 async def get_user_friends(user_id: str):
     user = await user_service.get_user_by_telegram_user_id(user_id)
-    return user
+    return await user.friends.all()
