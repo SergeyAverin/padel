c34ed9212bb08d83adf6c09a3dbc5eedefc1f74d
SergeyAverin <ct.averin@yandex.ru>
2024-06-27 22:52:55 +0600

    fix(backend): fixed auth import

--- backend/authentication/routers.py
+++ backend/authentication/routers.py
@@ -2,12 +2,9 @@ from logging import getLogger
 from aiogram.utils.web_app import safe_parse_webapp_init_data
 from fastapi import APIRouter, status, Body
 
-from services.user_service import user_service
-from utils.jwt_utils import encode_jwt
-from utils.generate_code import generate_code
-from schemas.user_schemas import UserDTO
-from config.bot_setting import bot_settings
-from exceptions.app_exceptions import DocumentNotFound
+from authentication.utils import encode_jwt
+from account.service import user_service
+from core.config.bot_settings import bot_settings
 
 
 auth_router = APIRouter(prefix='/auth', tags=['Auth'])
@@ -23,17 +20,9 @@ def login_jwt(user_id: str = Body()):
         user_id = str(data.user.id)
     except ValueError:
         return {'status': status.HTTP_401_UNAUTHORIZED, 'message': 'fail auth'}
-    user = None
-    try:
-        user = user_service.find_user_by_user_id(user_id)
-    except DocumentNotFound:
-        user = UserDTO(
-            nickname='anonim',
-            user_id=user_id,
-            user_code=generate_code(4),
-            type='user'
-        )
-        user = user_service.create_user(user)
+
+    user = user_service.find_user_by_user_id(user_id)
+
     if user:
         payload = {
             "sub": user.user_id
