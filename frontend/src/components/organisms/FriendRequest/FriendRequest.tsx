de87c4e2ef3d6e2cf0df6def337a8e63f28e3f8e
SergeyAverin <ct.averin@yandex.ru>
2024-06-29 00:28:42 +0600

    feat(frontend): added friend request component

---
+++ frontend/src/components/organisms/FriendRequest/FriendRequest.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import TestPhoto from "@assets/TestPhoto.png";
+import { Button, ButtonVariant } from "@atoms/index";
+
+export const FriendRequest: React.FC = () => {
+  return (
+    <div className="bg-primary p-5 rounded-2xl">
+      <div className="flex">
+        <div>
+          <img src={TestPhoto} className="rounded-full w-[72px] h-[72px]" />
+        </div>
+        <div className="ml-5">
+          <div className="text-[16px] font-bold">Username</div>
+          <div className="text-[16px] font-medium">Sergey averin</div>
+        </div>
+      </div>
+      <div className="flex mt-5">
+        <div className="w-[140px]">
+          <Button variant={ButtonVariant.FULL_HIGHLIGHT}>Accept</Button>
+        </div>
+        <div className="w-[140px] ml-3">
+          <Button variant={ButtonVariant.OUTLINED}>Reject</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
---
+++ frontend/src/components/organisms/FriendRequest/index.ts
@@ -0,0 +1,1 @@
+export { FriendRequest as default } from "./FriendRequest";
