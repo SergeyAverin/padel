d07bb5f53834fea0320efdc18f9f9bd4616650a5
SergeyAverin <ct.averin@yandex.ru>
2024-06-30 16:11:59 +0600

    feat(frontend): added data from server in padel info component

--- frontend/src/components/organisms/account/PadelInfo/PadelInfo.tsx
+++ frontend/src/components/organisms/account/PadelInfo/PadelInfo.tsx
@@ -1,23 +1,41 @@
 import React from "react";
+import { observer } from "mobx-react-lite";
 
-import HandIcon from "@assets/LeftHandIcon.svg?react";
+import { Hand, Position } from "@schemas/user";
+import UserStore from "@store/user";
+
+import LeftHandIcon from "@assets/LeftHandIcon.svg?react";
+import RightHandIcon from "@assets/RightHandIcon.svg?react";
+
+import LeftIcon from "@assets/LeftIcon.svg?react";
 import BothIcon from "@assets/BothIcon.svg?react";
+import RightIcon from "@assets/RightIcon.svg?react";
 
-export const PadelInfo: React.FC = () => {
+export const PadelInfo: React.FC = observer(() => {
   return (
     <div className="bg-primary p-5 rounded-md flex justify-around items-start">
       <div className="flex flex-col items-center">
         <div className="text-[36px] font-bold">
-          <HandIcon />
+          {UserStore.user?.hand == Hand.LEFT_HAND && <LeftHandIcon />}
+          {UserStore.user?.hand == Hand.RIGHT_HAND && <RightHandIcon />}
         </div>
-        <div className="text-[16px] font-medium mt-[15px]">Right hand</div>
+        <div className="text-[16px] font-medium mt-[15px]">
+          {UserStore.user?.hand == Hand.LEFT_HAND && "Left hand"}
+          {UserStore.user?.hand == Hand.RIGHT_HAND && "Right hand"}
+        </div>
       </div>
       <div className="flex flex-col items-center">
         <div className="text-[36px] font-bold">
-          <BothIcon />
+          {UserStore.user?.position == Position.LEFT && <LeftIcon />}
+          {UserStore.user?.position == Position.BOTH && <BothIcon />}
+          {UserStore.user?.position == Position.RIGHT && <RightIcon />}
         </div>
-        <div className="text-[16px] font-medium mt-[15px]">Both position</div>
+        <div className="text-[16px] font-medium mt-[15px]">
+          {UserStore.user?.position == Position.LEFT && "Left position"}
+          {UserStore.user?.position == Position.BOTH && "Both position"}
+          {UserStore.user?.position == Position.RIGHT && "Right position"}
+        </div>
       </div>
     </div>
   );
-};
+});
