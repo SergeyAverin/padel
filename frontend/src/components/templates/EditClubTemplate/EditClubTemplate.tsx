c53c0e14a838299cd19409ec1b3a7e582a7e3ee2
SergeyAverin <ct.averin@yandex.ru>
2024-07-13 15:21:06 +0600

    feat(frontend): added tabs in edit club

--- frontend/src/components/templates/EditClubTemplate/EditClubTemplate.tsx
+++ frontend/src/components/templates/EditClubTemplate/EditClubTemplate.tsx
@@ -1,4 +1,5 @@
 import { Heading, HeadingVariant } from "@atoms/index";
+import Tabs from "@molecules/Tabs";
 import DeleteClubForm from "@organisms/clubs/DeleteClubForm";
 import EditClubPhotos from "@organisms/clubs/EditClubPhotos";
 import UpdateClubForm from "@organisms/clubs/UpdateClubForm";
@@ -6,21 +7,31 @@ import UploadClubPhoto from "@organisms/clubs/UploadClubPhoto";
 import React from "react";
 
 export const EditClubTemplate: React.FC = () => {
+  const tabs = [
+    {
+      to: "#edit_club",
+      text: "Edit club data",
+      content: <UpdateClubForm />,
+    },
+    {
+      to: "#edit_club_avatar",
+      text: "Edit club avatar",
+      content: <UploadClubPhoto />,
+    },
+    {
+      to: "#edit_photos",
+      text: "Edit club photos",
+      content: <EditClubPhotos />,
+    },
+    {
+      to: "#delete_club",
+      text: "Delete club",
+      content: <DeleteClubForm />,
+    },
+  ];
   return (
     <div className="p-5">
-      <Heading variant={HeadingVariant.H1}>Edit club</Heading>
-      <div className="mt-5 mb-5">
-        <UpdateClubForm />
-      </div>
-      <div className="mt-5 mb-5">
-        <EditClubPhotos />
-      </div>
-      <div className="mt-5 mb-5">
-        <UploadClubPhoto />
-      </div>
-      <div>
-        <DeleteClubForm />
-      </div>
+      <Tabs subTab={tabs} />
     </div>
   );
 };
--- frontend/src/components/templates/EditProfileTemplate/EditProfileTemplate.tsx
+++ frontend/src/components/templates/EditProfileTemplate/EditProfileTemplate.tsx
@@ -1,3 +1,4 @@
+import Tabs from "@molecules/Tabs";
 import EditProfileForm from "@organisms/account/EditProfileForm";
 import SelectHand from "@organisms/account/SelectHand";
 import SelectPosition from "@organisms/account/SelectPosition";
@@ -5,16 +6,34 @@ import UploadPhotoForm from "@organisms/account/UploadPhotoForm";
 import React from "react";
 
 export const EditProfileTemplate: React.FC = () => {
+  const tabs = [
+    {
+      to: "#edit_profile_data",
+      text: "Edit profile data",
+      content: <EditProfileForm />,
+    },
+    {
+      to: "#upload_avatar",
+      text: "Upload avatar",
+      content: <UploadPhotoForm />,
+    },
+    {
+      to: "#edit_padel_data",
+      text: "Edit padel data",
+      content: (
+        <>
+          {" "}
+          <SelectHand />
+          <div className="mt-3">
+            <SelectPosition />
+          </div>
+        </>
+      ),
+    },
+  ];
   return (
     <div className="p-3 mb-[100px]">
-      <EditProfileForm />
-      <div className="mt-3">
-        <UploadPhotoForm />
-      </div>
-      <SelectHand />
-      <div className="mt-3">
-        <SelectPosition />
-      </div>
+      <Tabs subTab={tabs} />
     </div>
   );
 };
