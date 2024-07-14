005df39477bb0874217eadf4118094aa15caf2d3
SergeyAverin <ct.averin@yandex.ru>
2024-07-14 13:34:54 +0600

    feat(frontend): added tabs in match template

--- frontend/src/components/templates/MatchTemplate/MatchTemplate.tsx
+++ frontend/src/components/templates/MatchTemplate/MatchTemplate.tsx
@@ -5,8 +5,67 @@ import MatchStore from "@store/match";
 import { Heading, HeadingVariant, Spinner } from "@atoms/index";
 import Match from "@organisms/matches/Match";
 import { EmptyBanner } from "@organisms/EmptyBanner/EmptyBanner";
+import Tabs from "@molecules/Tabs";
 
 export const MatchTemplate: React.FC = observer(() => {
+  const tabs = [
+    {
+      to: "#your",
+      text: "Your",
+      content: (
+        <>
+          {MatchStore.matches.length != 0 && (
+            <div>
+              <Heading variant={HeadingVariant.H2}>Your match</Heading>
+              {MatchStore.matches.map((match) => (
+                <div className="mt-3" key={match.id}>
+                  <Match />
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      ),
+    },
+    {
+      to: "#friends",
+      text: "Friends",
+      content: (
+        <>
+          {MatchStore.matchesFromFriends.length != 0 && (
+            <div className="mt-5">
+              <Heading variant={HeadingVariant.H2}>Match from friends</Heading>
+              {MatchStore.matchesFromFriends.map((match) => (
+                <div className="mt-3" key={match.id}>
+                  <Match />
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      ),
+    },
+    {
+      to: "#clubs",
+      text: "Clubs",
+      content: (
+        <>
+          {MatchStore.matchesFromBookmarks.length != 0 && (
+            <div className="mt-5">
+              <Heading variant={HeadingVariant.H2}>
+                Match from bookmarked clubs
+              </Heading>
+              {MatchStore.matchesFromBookmarks.map((match) => (
+                <div className="mt-3" key={match.id}>
+                  <Match />
+                </div>
+              ))}
+            </div>
+          )}
+        </>
+      ),
+    },
+  ];
   return (
     <div className="p-5">
       {MatchStore.isLoading ? (
@@ -22,40 +81,7 @@ export const MatchTemplate: React.FC = observer(() => {
             </>
           ) : (
             <>
-              {MatchStore.matches.length != 0 && (
-                <div>
-                  <Heading variant={HeadingVariant.H2}>Your match</Heading>
-                  {MatchStore.matches.map((match) => (
-                    <div className="mt-3" key={match.id}>
-                      <Match />
-                    </div>
-                  ))}
-                </div>
-              )}
-              {MatchStore.matchesFromFriends.length != 0 && (
-                <div className="mt-5">
-                  <Heading variant={HeadingVariant.H2}>
-                    Match from friends
-                  </Heading>
-                  {MatchStore.matchesFromFriends.map((match) => (
-                    <div className="mt-3" key={match.id}>
-                      <Match />
-                    </div>
-                  ))}
-                </div>
-              )}
-              {MatchStore.matchesFromBookmarks.length != 0 && (
-                <div className="mt-5">
-                  <Heading variant={HeadingVariant.H2}>
-                    Match from bookmarked clubs
-                  </Heading>
-                  {MatchStore.matchesFromBookmarks.map((match) => (
-                    <div className="mt-3" key={match.id}>
-                      <Match />
-                    </div>
-                  ))}
-                </div>
-              )}
+              <Tabs subTab={tabs} />
             </>
           )}
         </>
