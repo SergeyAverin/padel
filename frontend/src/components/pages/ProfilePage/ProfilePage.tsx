6bd3bb7d5a657f15689bdcb980dd5b840b2809d6
SergeyAverin <ct.averin@yandex.ru>
2024-07-14 12:08:57 +0600

    fix(frontend): fixed loading profile after user page

--- frontend/src/components/pages/ProfilePage/ProfilePage.tsx
+++ frontend/src/components/pages/ProfilePage/ProfilePage.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { observer } from "mobx-react-lite";
 import ProfileTemplate from "@templates/ProfileTemplate";
-import UserStore from "@store/user";
+import AuthStore from "@store/auth";
 import { Spinner } from "@atoms/index";
 
 export const ProfilePage: React.FC = observer(() => {
-  return <>{UserStore.user ? <ProfileTemplate /> : <Spinner />}</>;
+  return <>{AuthStore.authUser ? <ProfileTemplate /> : <Spinner />}</>;
 });
