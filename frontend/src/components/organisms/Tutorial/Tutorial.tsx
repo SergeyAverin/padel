ebce87ca375e930f6b6ed03d192c2b0556be7e13
SergeyAverin <ct.averin@yandex.ru>
2024-07-26 14:57:21 +0600

    feat(frontend): added select lvl in tutorial

--- frontend/src/components/organisms/Tutorial/Tutorial.tsx
+++ frontend/src/components/organisms/Tutorial/Tutorial.tsx
@@ -1,4 +1,5 @@
-import { Button, ButtonVariant } from "@atoms/index";
+import { Button, ButtonVariant, Heading, HeadingVariant } from "@atoms/index";
+import Select from "@atoms/Select";
 import { useAnimation } from "@hooks/useAnimation";
 import SelectHand from "@organisms/account/SelectHand";
 import SelectPosition from "@organisms/account/SelectPosition";
@@ -23,6 +24,26 @@ export const Slide: React.FC<ISlideProps> = ({ animation, text, title }) => {
   );
 };
 
+interface LvlDescriptionProps extends React.PropsWithChildren {
+  title: string;
+  lvl: string;
+}
+const LvlDescription: React.FC<LvlDescriptionProps> = ({
+  children,
+  lvl,
+  title,
+}) => {
+  return (
+    <div className="mb-3">
+      <div className="text-[24px] w-[34px] h-[34px] mb-1 rounded-full p-1 bg-highlight text-bg flex items-center justify-center">
+        {lvl}
+      </div>
+      <div className="text-[24px] mb-1">{title}</div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
 export const Tutorial: React.FC = () => {
   const [step, setStep] = useState(1);
   const [isOpen, setIsOpen] = useState(false);
@@ -41,6 +62,50 @@ export const Tutorial: React.FC = () => {
       <div className="flex justify-center">
         <div className="w-[280px text-center]">
           <SelectPosition />
+        </div>
+      </div>
+    </div>,
+    <div>
+      <div className="flex justify-center">
+        <div className="w-full p-5">
+          <Heading variant={HeadingVariant.H1}>Select lvl</Heading>
+          <div className="w-[300px] m-auto mb-5">
+            <Select
+              defaultValue={{ label: "1 - initiation", value: "1" }}
+              options={[
+                { label: "1 - initiation", value: "1" },
+                { label: "2 - initiation", value: "2" },
+                { label: "3 - initiation", value: "3" },
+                { label: "4 - initiation", value: "4" },
+                { label: "5 - initiation", value: "5" },
+                { label: "6 - initiation", value: "6" },
+                { label: "7 - initiation", value: "7" },
+                { label: "8 - initiation", value: "8" },
+                { label: "9 - initiation", value: "9" },
+                { label: "10 - initiation", value: "10" },
+              ]}
+            />
+          </div>
+          <div className="overflow-y-scroll h-[300px] ">
+            <LvlDescription lvl="1" title="initiation">
+              I am interested in the rules I'm starting to play
+            </LvlDescription>
+
+            <LvlDescription lvl="2" title="Débutant">
+              I'm starting to learn the basic moves I know the rules The game is
+              slow, the games are short
+            </LvlDescription>
+
+            <LvlDescription lvl="3" title="Beginner+">
+              I play 1 time a month I'm starting to pull off a few lobs I'm
+              starting to play with the windows
+            </LvlDescription>
+
+            <LvlDescription lvl="3" title="Beginner+">
+              I play 1 time a month I'm starting to pull off a few lobs I'm
+              starting to play with the windows
+            </LvlDescription>
+          </div>
         </div>
       </div>
     </div>,
