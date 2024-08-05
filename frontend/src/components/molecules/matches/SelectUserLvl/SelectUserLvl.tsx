cfdc50aea977a4454c7c87fc1f6a5f900685f354
SergeyAverin <ct.averin@yandex.ru>
2024-08-05 13:20:05 +0600

    feat(frontedn): fixed blank

--- frontend/src/components/molecules/matches/SelectUserLvl/SelectUserLvl.tsx
+++ frontend/src/components/molecules/matches/SelectUserLvl/SelectUserLvl.tsx
@@ -1,33 +1,36 @@
 import { Heading, HeadingVariant } from "@atoms/index";
 import Select from "@atoms/Select";
+import { Option } from "@atoms/Select/selectOption";
 import React from "react";
 
 interface SelectUserLvlProps {
   title: string;
   onChange: (option: { label: string; value: string }) => void;
+  value: Option;
 }
 
 export const SelectUserLvl: React.FC<SelectUserLvlProps> = ({
   title,
   onChange,
+  value,
 }) => {
   return (
     <>
       <Heading variant={HeadingVariant.H2}>{title}</Heading>
       <Select
         onChange={onChange}
-        defaultValue={{ label: "1", value: "1" }}
+        defaultValue={value}
         options={[
-          { label: "1", value: "1" },
-          { label: "2", value: "2" },
-          { label: "3", value: "3" },
-          { label: "4", value: "4" },
-          { label: "5", value: "5" },
-          { label: "6", value: "6" },
-          { label: "7", value: "7" },
-          { label: "8", value: "8" },
-          { label: "9", value: "9" },
-          { label: "10", value: "10" },
+          { label: "1 | Initiation", value: "-1.5" },
+          { label: "2 | Beginner", value: "-1" },
+          { label: "3 | Beginner+", value: "-0.75" },
+          { label: "4 | Intermediate", value: "-0.5" },
+          { label: "5 | Intermediate+", value: "-0.25" },
+          { label: "6 | Advanded", value: "0" },
+          { label: "7 | Advanded+", value: "0.25" },
+          { label: "8 | Expert", value: "0.75" },
+          { label: "9 | Expert+", value: "1" },
+          { label: "10 | Elite", value: "1.5" },
         ]}
       />
     </>
--- frontend/src/components/organisms/matches/Blank/Blank.tsx
+++ frontend/src/components/organisms/matches/Blank/Blank.tsx
@@ -1,4 +1,4 @@
-import { Button, ButtonVariant } from "@atoms/index";
+import { Button, ButtonVariant, Label } from "@atoms/index";
 import SelectUserLvl from "@molecules/matches/SelectUserLvl";
 import { IMatch } from "@schemas/match";
 import Match from "@organisms/matches/Match";
@@ -10,39 +10,58 @@ interface IBlankProps {
 }
 
 export const Blank: React.FC<IBlankProps> = ({ match }) => {
-  const [user1, setUser1] = useState(1);
-  const [user2, setUser2] = useState(1);
-  const [user3, setUser3] = useState(1);
-  const [user4, setUser4] = useState(1);
+  const [user1, setUser1] = useState({ label: "1", value: "1" });
+  const [user2, setUser2] = useState({ label: "1", value: "1" });
+  const [user3, setUser3] = useState({ label: "1", value: "1" });
+  const [user4, setUser4] = useState({ label: "1", value: "1" });
   const onSubmit = () => {
-    BlankStore.createBlank(match.id, user1, user2, user3, user4);
+    BlankStore.createBlank(
+      match.id,
+      Number(user1.value),
+      Number(user2.value),
+      Number(user3.value),
+      Number(user4.value)
+    );
   };
   return (
     <div className="p-5">
-      <div>
-        <Match match={match} />
-      </div>
-      <SelectUserLvl
-        title="User 1"
-        onChange={(option) => setUser1(Number(option.value))}
-      />
-      <div className="mt-3">
-        <SelectUserLvl
-          title="User 2"
-          onChange={(option) => setUser2(Number(option.value))}
-        />
-      </div>
-      <div className="mt-3">
-        <SelectUserLvl
-          title="User 3"
-          onChange={(option) => setUser3(Number(option.value))}
-        />
-      </div>
-      <div className="mt-3">
-        <SelectUserLvl
-          title="User 4"
-          onChange={(option) => setUser4(Number(option.value))}
-        />
+      <div className="grid grid-cols-2 gap-4">
+        {match.user_1 && (
+          <div>
+            <SelectUserLvl
+              value={user1}
+              title={match.user_1.username}
+              onChange={(option) => setUser1(option)}
+            />
+          </div>
+        )}
+        {match.user_2 && (
+          <div>
+            <SelectUserLvl
+              value={user2}
+              title={match.user_2.username}
+              onChange={(option) => setUser2(option)}
+            />
+          </div>
+        )}
+        {match.user_3 && (
+          <div>
+            <SelectUserLvl
+              value={user3}
+              title={match.user_3.username}
+              onChange={(option) => setUser3(option)}
+            />
+          </div>
+        )}
+        {match.user_4 && (
+          <div>
+            <SelectUserLvl
+              title={match.user_4.username}
+              value={user4}
+              onChange={(option) => setUser4(option)}
+            />
+          </div>
+        )}
       </div>
       <div className="mt-5">
         <Button variant={ButtonVariant.FULL_HIGHLIGHT} onClick={onSubmit}>
