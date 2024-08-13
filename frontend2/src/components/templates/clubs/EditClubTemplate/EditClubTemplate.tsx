6183e40621878ee3ab393671a8af8b0b94643bdb
SergeyAverin <ct.averin@yandex.ru>
2024-08-13 12:00:18 +0600

    feat(frontend): added edit club page

--- frontend2/src/components/templates/clubs/EditClubTemplate/EditClubTemplate.tsx
+++ frontend2/src/components/templates/clubs/EditClubTemplate/EditClubTemplate.tsx
@@ -1,5 +1,37 @@
+import Tabs from "@molecules/core/Tabs";
 import React from "react";
 
 export const EditClubTemplate: React.FC = () => {
-  return <>sadf</>;
+  const tabs = [
+    {
+      to: "#edit_club",
+      text: "Data",
+      content: 123,
+    },
+    {
+      to: "#edit_club_avatar",
+      text: "Avatar",
+      content: 123,
+    },
+    {
+      to: "#edit_photos",
+      text: "Photos",
+      content: 123,
+    },
+    {
+      to: "#delete_club",
+      text: "Delete",
+      content: 1123,
+    },
+    {
+      to: "#edit_courts",
+      text: "Courts",
+      content: 123,
+    },
+  ];
+  return (
+    <>
+      <Tabs subTab={tabs} />
+    </>
+  );
 };
