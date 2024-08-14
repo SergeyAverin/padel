90572a429932dfac218d4d84a33fab12f2e9c7a5
SergeyAverin <ct.averin@yandex.ru>
2024-08-14 16:23:34 +0600

    feat(frontend): added fade in animation

--- frontend2/tailwind.config.js
+++ frontend2/tailwind.config.js
@@ -13,6 +13,17 @@ export default {
       highlight: '#97E7E1',
       grey: '#A2ACB0',
       error: '#F54747'
+    },
+    extend: {
+      keyframes: {
+        'fade-in': {
+          '0%': { opacity: '0' },
+          '100%': { opacity: '1' },
+        },
+      },
+      animation: {
+        'fade-in': 'fade-in 0.3s ease-in-out',
+      },
     }
   },
   plugins: [],
