66672f2b9952184f9e731231d0f402b66b6e1580
$ergeyAverin <ct.averin@yandex.ru>
2024-12-02 21:19:36 +0600

    Update geoSlice.ts

--- frontend2/src/redux/features/geoSlice.ts
+++ frontend2/src/redux/features/geoSlice.ts
@@ -9,16 +9,34 @@ const initialState: IGeoState = {
   lat: null,
   lon: null,
 };
-
+interface ILocationData {
+  latitude: number;
+  longitude: number;
+}
 export const GeoSlice = createSlice({
   name: "Geo",
   initialState,
   reducers: {
     setGeo(state) {
-      navigator.geolocation.getCurrentPosition((position) => {
-        state.lat = position.coords.latitude;
-        state.lon = position.coords.longitude;
-      });
+      if (window.Telegram.WebApp.platform == "tdesktop") {
+        navigator.geolocation.getCurrentPosition((position) => {
+          state.lat = position.coords.latitude;
+          state.lon = position.coords.longitude;
+        });
+      } else {
+        // @ts-ignore
+        window.Telegram.WebApp.LocationManager.init(() => {
+          // @ts-ignore
+          window.Telegram.WebApp.LocationManager.getLocation(
+            (data: null | ILocationData) => {
+              if (data) {
+                state.lat = data.latitude;
+                state.lon = data.longitude;
+              }
+            }
+          );
+        });
+      }
     },
   },
 });
