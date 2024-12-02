98b5b10cad7f1503f20d414a08959e1b52cb74a9
SergeyAverin <ct.averin@yandex.ru>
2024-12-02 17:20:30 +0600

    fix(frontend): fix app

--- frontend2/src/App.tsx
+++ frontend2/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import MainRouter from "./routers";
+import { store } from "@redux/store";
+
 const App = () => {
-  return <div className="text-fg p-2 pb-[150px]">test</div>;
+  return (
+    <div className="text-fg p-2 pb-[150px]">
+      <Provider store={store}>
+        <BrowserRouter>
+          <MainRouter />
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
 };
 
 export default App;
